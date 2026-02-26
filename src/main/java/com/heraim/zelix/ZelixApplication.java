de305f132078347b84b464d63fcfd2fb3197a4ac
dave-programmer01 <satpasser@gmail.com>
2026-02-26 12:13:47 -0500

    Added project files

---
+++ src/main/java/com/heraim/zelix/ZelixApplication.java
@@ -0,0 +1,13 @@
+package com.heraim.zelix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZelixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZelixApplication.class, args);
+    }
+
+}
